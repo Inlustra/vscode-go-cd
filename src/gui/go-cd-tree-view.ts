e7d50a1f93dd3e20cfc4dad9fd4a731fb7950ec4
Thomas Nairn <thomas@thenairn.com>
2018-07-15 09:16:12 +0100

    Fixed tree collapsible state when the stage didn't run any jobs

--- src/gui/go-cd-tree-view.ts
+++ src/gui/go-cd-tree-view.ts
@@ -48,101 +48,109 @@ export class GoCdTreeView implements TreeDataProvider<GoCdTreeNode> {
 
   getTreeItem(element: GoCdTreeNode): TreeItem | Thenable<TreeItem> {
     try {
-    if (!!element.job) {
-      const treeItem = new TreeItem(element.job.name)
-      treeItem.iconPath = this.getIconFromStage(element.job.result)
-      return treeItem
-    } else if (element.stage) {
-      const treeItem = new TreeItem(
-        element.stage.name,
-        TreeItemCollapsibleState.Collapsed
-      )
-      treeItem.iconPath = this.getIconFromStages(
-        element.stage.jobs.map(job => job.result)
-      )
-      return treeItem
-    } else if (element.history) {
-      const treeItem = new TreeItem(
-        element.history.label,
-        TreeItemCollapsibleState.Collapsed
-      )
-      treeItem.iconPath = this.getIconFromStages(
-        element.history.stages.map(stage => stage.result)
-      )
-      return treeItem
-    } else if (element.pipeline) {
-      const { pipeline } = element
-      let label = pipeline.name
-      const instance = pipeline._embedded.instances.slice(-1).pop()
-      label += ` - ${(instance && instance.label) || 'Not yet run'}`
-      const treeItem = new TreeItem(label, TreeItemCollapsibleState.Collapsed)
-      treeItem.iconPath =
-        instance &&
-        this.getIconFromStages(
-          instance._embedded.stages.map(stage => stage.status)
+      if (!!element.job) {
+        const treeItem = new TreeItem(element.job.name)
+        treeItem.iconPath = this.getIconFromStage(element.job.result)
+        return treeItem
+      } else if (element.stage) {
+        const treeItem = new TreeItem(
+          element.stage.name,
+          element.stage.jobs.length
+            ? TreeItemCollapsibleState.Collapsed
+            : TreeItemCollapsibleState.None
         )
-      return treeItem
-    } else if (element.group) {
-      return new TreeItem(
-        element.group.name,
-        TreeItemCollapsibleState.Collapsed
-      )
+        treeItem.iconPath = this.getIconFromStages(
+          element.stage.jobs.map(job => job.result)
+        )
+        return treeItem
+      } else if (element.history) {
+        const treeItem = new TreeItem(
+          element.history.label,
+          TreeItemCollapsibleState.Collapsed
+        )
+        treeItem.iconPath = this.getIconFromStages(
+          element.history.stages.map(stage => stage.result)
+        )
+        return treeItem
+      } else if (element.pipeline) {
+        const { pipeline } = element
+        let label = pipeline.name
+        const instance = pipeline._embedded.instances.slice(-1).pop()
+        label += ` - ${(instance && instance.label) || 'Not yet run'}`
+        const treeItem = new TreeItem(label, TreeItemCollapsibleState.Collapsed)
+        treeItem.iconPath =
+          instance &&
+          this.getIconFromStages(
+            instance._embedded.stages.map(stage => stage.status)
+          )
+        return treeItem
+      } else if (element.group) {
+        return new TreeItem(
+          element.group.name,
+          TreeItemCollapsibleState.Collapsed
+        )
+      }
+    } catch (e) {
+      console.error(e)
     }
-  }catch(e){console.error(e)}
-  return new TreeItem('Loading...')
+    return new TreeItem('Loading...')
   }
 
   getChildren(
     element?: GoCdTreeNode | undefined
   ): ProviderResult<GoCdTreeNode[]> {
-    if (this.groups === null) {
-      return null
-    }
-    if (!element) {
-      return this.groups.map(group => ({ group }))
-    } else if (!!element.stage) {
-      return element.stage.jobs.map(job => ({
-        group: element.group,
-        pipeline: element.pipeline,
-        history: element.history,
-        stage: element.stage,
-        job
-      }))
-    } else if (!!element.history) {
-      return element.history.stages.map(stage => ({
-        group: element.group,
-        pipeline: element.pipeline,
-        history: element.history,
-        stage
-      }))
-    } else if (!!element.pipeline) {
-      const name = element.pipeline.name
-      return State.getPipelineHistory(name)
-        .pipe(
-          first(),
-          map(paginated =>
-            paginated.pipelines.map(history => ({
-              group: element.group,
-              pipeline: element.pipeline,
-              history
-            }))
-          ),
-          catchError(e => {
-            showErrorAlert(
-              e,
-              `Error loading pipeline history for: ${name}`,
-              REFRESH_PIPELINES,
-              OK
-            )
-            return []
-          })
-        )
-        .toPromise()
-    } else if (!!element.group) {
-      return element.group._embedded.pipelines.map(pipeline => ({
-        group: element.group,
-        pipeline
-      }))
+    try {
+      if (this.groups === null) {
+        return null
+      }
+      if (!element) {
+        return this.groups.map(group => ({ group }))
+      } else if (!!element.stage) {
+        return element.stage.jobs.map(job => ({
+          group: element.group,
+          pipeline: element.pipeline,
+          history: element.history,
+          stage: element.stage,
+          job
+        }))
+      } else if (!!element.history) {
+        return element.history.stages.map(stage => ({
+          group: element.group,
+          pipeline: element.pipeline,
+          history: element.history,
+          stage
+        }))
+      } else if (!!element.pipeline) {
+        const name = element.pipeline.name
+        return State.getPipelineHistory(name)
+          .pipe(
+            first(),
+            map(paginated =>
+              paginated.pipelines.map(history => ({
+                group: element.group,
+                pipeline: element.pipeline,
+                history
+              }))
+            ),
+            catchError(e => {
+              showErrorAlert(
+                e,
+                `Error loading pipeline history for: ${name}`,
+                REFRESH_PIPELINES,
+                OK
+              )
+              return []
+            })
+          )
+          .toPromise()
+      } else if (!!element.group) {
+        return element.group._embedded.pipelines.map(pipeline => ({
+          group: element.group,
+          pipeline
+        }))
+      }
+    } catch (e) {
+      console.log(e)
     }
   }
 
@@ -156,11 +164,10 @@ export class GoCdTreeView implements TreeDataProvider<GoCdTreeNode> {
         return Icons.sync
       case 'Cancelled':
         return Icons.ban
-        case 'Unknown':
-        return Icons.question
-      default:
+      case 'Unknown':
       case null:
       case undefined:
+      default:
         return undefined
     }
   }
@@ -169,9 +176,6 @@ export class GoCdTreeView implements TreeDataProvider<GoCdTreeNode> {
     return stages
       .map((stage, idx, arr) => {
         const icon = this.getIconFromStage(stage)
-        if(icon === undefined && (stage !== undefined || stage !== null)) {
-          console.log(stage)
-        }
         if (icon === Icons.check && idx === arr.length - 1) {
           return Icons.checkDouble
         }
